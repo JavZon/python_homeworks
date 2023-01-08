effedecae7ad5b7670df9c525f63d46d24b81a2e
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-08 18:44:42 +0900

    for loop mavzusida uy ishi
    or loop mavzusida  bajarilgan uy ishlarim

---
+++ for_loop_bo'yicha_uy_ishi.py
@@ -0,0 +1,45 @@
+
+#uy ishi for loop mavzusida
+cars=['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+for a in cars:
+    if a=='gm':
+        print(a.upper())
+    else:
+        print(a.title())
+        
+    
+cars=['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+for a in cars:
+    if a!='gm':
+        print(a.title())
+    else:
+        print(a.upper())
+        
+
+cars=['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+for a in cars:
+    if a!='gm':
+        print(a.upper())
+    else:
+        print(a.title())
+
+
+
+login=input("Iltimos loginingizni kiriting : ")
+if login.lower()=='admin':
+    print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi ?")
+else:
+    print(f" Xush kelibsiz, {login} ! " )
+    
+
+
+
+
+
+
+
+
+
+
+
+
