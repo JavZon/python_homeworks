6fbaf93b89622de0f1bf879cd8e9b3107e7178f8
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-09 10:49:10 +0900

    14-dars. Dictionary
    Dars davomida yozilgan kodlar.

---
+++ 14-dars_dictionary.py
@@ -0,0 +1,31 @@
+#14-dars. dictionary
+#car={'model':'mercedes benz', 'color':'black'}
+#print(car['model'])
+#print(f"The color of this car is {car['color']}")
+
+#mevalar={'olma':5000, 'anor':7000, 'qovun':10000}
+#print(mevalar['qovun'])
+
+#student={'name':"temur qobilov", "age":25, "born_year":1994}
+#print(f"{student['name'].title()}, \
+#{student['age']} years old, \
+#was born in {student['born_year']}")
+
+#talaba={'ism':'murod', 'yosh':20}
+#talaba['kurs']=3
+#talaba['faculty']='information and technology'
+
+#talaba['ism']='bobur'
+
+
+#mevalar={'olma':5000, 'anor':7000, 'qovun':10000}
+#del mevalar['qovun']
+#print(mevalar)
+
+phones={
+        'ali':'iphone X',
+        'vali':'Samsung 10s',
+        'olim': 'redmi 10'
+        }
+
+print(phones)
