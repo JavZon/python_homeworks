a36ed2f9fab701a5b19f908a8c93a6e8e5516e44
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-22 15:16:31 +0900

    Update 20-dars.py

--- 20-dars.py
+++ 20-dars.py
@@ -1,10 +1,10 @@
 #20-dars. qiymat qaytaruvchi funksiya
 
 #def son(a,b):
- #   multiple= a*b
-  #  return multiple
+  #  multiple= a*b
+ #   return multiple
 
-#w=son(2,3)
+#w=son(22,30)
 #print(w)
 
 #def student_name(ism,familiya):
@@ -24,27 +24,27 @@
     #    yigindi=x+y
     #return yigindi    
 
-#son1=raqamlar_yig(5,2,4)
-#son2=raqamlar_yig(6,7)
+#son1=raqamlar_yig(9,5,2)
+#son2=raqamlar_yig(14,31)
 #print(f" Son1 ning yigindisi- {son1}. Son2 ning yig'indisi - {son2}")
 
 
 #def raqamlar_yig(x,y,z=10):
  #   if z:
-  #      yigindi=x+y+z
+  #     yigindi=x+y+z
    # else:
     #    yigindi=x+y+10
     #return yigindi    
 
-#son1=raqamlar_yig(3,11,4)
-#son2=raqamlar_yig(6,7)
+#son1=raqamlar_yig(25,11,3)
+#son2=raqamlar_yig(9,51)
 #print(f" Son1 ning yigindisi- {son1}. Son2 ning yig'indisi - {son2}")
 
 
 #def avto_salon(company,model,color,year,price=None):
  #   car={
-   #     'kompaniya_nomi':company,
-  #      'mash_modeli':model,
+  #      'kompaniya_nomi':company,
+   #     'mash_modeli':model,
     #    'mash_rangi':color,
      #   'mash_yili':year,
       #  'mash_narhi':price
@@ -64,55 +64,79 @@
     #print(f"{b['mash_modeli']}, rangi- {b['mash_rangi']}. Narxi - {qiymat}")        
     
 
-
-
-
+ 
+#def yangi_range(son1,son2):
+    #sonlar=[]
+    #while son1<son2:
+   #     sonlar.append(son1)
+  #      son1+=1
+ #   return sonlar
     
-def yangi_range(son1,son2):
-    sonlar=[]
-    while son1<son2:
-        sonlar.append(son1)
-        son1+=1
-    return sonlar
-    
-b=yangi_range(2, 15) 
-print(b)
+#b=yangi_range(-4, 11) 
+#print(b)
     
 
-def oraliq(s1,s2,s3=''):
-    raqamlar=[]
-    while s1<s2:
-        raqamlar.append(s1)
-        if s3:
-            s1+=s3
-        else:
-            s1+=1
-    return raqamlar
+#def oraliq(s1,s2,s3=''):
+ #   raqamlar=[]
+  #  while s1<s2:
+   #     raqamlar.append(s1)
+    #    if s3:
+     #       s1+=s3
+      #  else:
+       #     s1+=1
+    #return raqamlar
 
-qw=oraliq(3,28,4)
-print(qw)
+#qw=oraliq(42,75,7)
+#print(qw)
             
-def y_o(n1,n2,n3=''):
-    num_bers=[]
-    while n1<n2:
-        if n3:
-            num_bers.append(n1)
-            n1+=n3
-        else:
-            num_bers.append(n1)
-            n1+=1
-    return num_bers
+#def y_o(n1,n2,n3=None):
+ #   num_bers=[]
+  #  while n1<n2:
+   #     if n3:
+    #        num_bers.append(n1)
+     #       n1+=n3
+      #  else:
+       #     num_bers.append(n1)
+        #    n1+=1
+    #return num_bers
 
-yyy=y_o(7,20)            
-print(yyy)
+#yyy=y_o(7,25)            
+#print(yyy)
 
 
+def avto_salon(company,model,color,year,price=None):
+    car={
+       'kompaniya_nomi':company,
+       'mash_modeli':model,
+       'mash_rangi':color,
+       'mash_yili':year,
+       'mash_narhi':price
+        }
 
-  
+    return car
+
+print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
+
+cars=[] # salondagi avtolar uchun bo'sh ro'yxat
+
+while True:
+    print("\nQuyidagi ma'lumotlarni kiriting",end='')
+    company=input("Ishlab chiqaruvchi: ")
+    model=input("Modeli: ")
+    color=input("Rangi: ")
+    year=input("Ishlab chiqarilgan yili: ")
+    price=input("Narhi: ")
+    
+    cars.append(avto_salon(company,model,color,year,price))
+    
+    savol = input("Yana avto qo'shasizmi? (yes/no): ")
+    if savol=='no':
+        break  
 
           
-    
- 
+print("Biz quyidagi avtomobillarni taqdim etamiz : ")    
+for a in cars:
+    print(f"{a['kompaniya_nomi']}. Modeli - {a['mash_modeli']}. Yili - {a['mash_yili']}. Narhi - {a['mash_narhi']}.")
     
  
     
