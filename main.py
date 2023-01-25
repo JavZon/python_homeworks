c06b3ef0b7a53812f0e8aae1a5a555daeb19b64c
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-25 23:34:03 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,108 @@
+
+# import avto_salon_modul as asm
+
+# a=asm.kopaytir(2, 3, 5)
+# b=asm.qoshish(5, 9, 3, 31, -1, -1 -3, 20)
+# print(a)
+# print(b)
+
+# avto1=asm.avto_salon('Mitsubishi', 'Watashiwas', 'Black', 2022, 52000)
+# asm.avto_print(avto1)
+
+# from avto_salon_modul import avto_salon, avto_print
+# car=avto_salon('Audi', 'Q7', 'Black', 2021, 35000)
+# avto_print(car)
+
+# from avto_salon_modul import qoshish
+
+# a=qoshish(2, 3, 4, 5, 3)
+# #print(a)
+
+# from avto_salon_modul import kopaytir
+# b=kopaytir(2, 3, 7,3,5)
+# #print(b)
+
+# print(b/a)
+#fayldan qisqa nom orqali funksiyani chaqirish. bunda fayldan funksiyani import qildik va uni kpt deb nomladik.
+# from avto_salon_modul import kopaytir as kpt
+
+# d=kpt(2, 3, 8)
+# print(d)
+
+# from avto_salon_modul import *
+
+# c=qoshish(3, 5, 7)
+# print(c)
+
+# b=kopaytir(2,6,5)
+# print(b)
+
+
+# from avto_salon_modul import ddd
+
+# a=qoshish(2,6,6,4)
+# print(a)
+
+# gg=ddd
+# print(gg)
+
+# import math
+
+# # print(math.sqrt(10))
+
+# # print(math.pow(7,4))
+
+# # print(math.pi)
+
+# # print(math.log(2,16))
+# # print(math.log2(8))
+# # print(math.log10(265))
+
+
+# print(math.ceil(26.32))
+# print(math.fabs(24.36))
+# print(math.floor(-645.01))
+# print(math.degrees(60))
+
+
+import random as r
+
+# print(r.randint(3, 85))
+# s=['olim',2,98, 62, 'biroq', 'ali', 'vali',52,963 ]
+# print(r.choice(s))
+
+# b=range(1,255)
+# print(r.choice(b))
+
+
+x=[0,1,2,3,4,5,6,7,8,9,10]
+print(x)
+r.shuffle(x)
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
