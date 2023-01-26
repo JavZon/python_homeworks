3036b1370cd2775beb12c9bd466492135de5af80
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-26 23:51:46 +0900

    24-dars. Funksiyalar.so'ngi so'z
    24-dars. Funksiyalar.so'ngi so'z

---
+++ 24-dars_ Funksiyalar_so'ngi_so'z.py
@@ -0,0 +1,34 @@
+# 24-dars. Funksiyalar.so'ngi so'z
+
+
+# a=lambda x,y:x+y
+# print(a(2,5))
+
+# from math import pi
+
+# b=lambda pi,r:2*pi*r
+# print(b(pi,10))
+
+# c=lambda x,y:x**y
+# print(c(2,5))
+
+# def daraja(n):
+#     return lambda a:a**n
+
+# print(daraja(3))
+
+
+sonlar
+
+
+
+
+
+
+
+
+
+
+
+
+
