a47e87428e23020c59d02c1c6b7d0e864a5549a3
Javokhir <122201059+JavZon@users.noreply.github.com>
2023-01-27 01:22:35 +0900

    Update 24-dars_ Funksiyalar_so'ngi_so'z.py

--- 24-dars_ Funksiyalar_so'ngi_so'z.py
+++ 24-dars_ Funksiyalar_so'ngi_so'z.py
@@ -17,10 +17,33 @@
 
 # print(daraja(3))
 
+# from math import sqrt
+# sonlar=list(range(11))
 
-sonlar
+# ildiz=list(map(sqrt,sonlar))
+# print(ildiz)
 
+# def daraja_kv(x):
+#     return x*x
+# sonlar=list(range(11))
+# # print(list(map(daraja_kv,sonlar)))
 
+# kv=list(map(lambda a:a*a, sonlar))
+# print(kv)
+
+# a=[2,5,8]
+# b=[4,6,7]
+
+# print(list(map(lambda x,y:x+y,a,b)))
+
+# ism=['hasan','husan','olim']
+# print(tuple(map(lambda a:a.upper(),ism)))
+
+# ismlar=[]
+# for a in ism:
+#     ismlar.append(a.upper())
+# print(ismlar)    
+    
 
 
 
